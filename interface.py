b08f5ae212037282fc572d76a047659aa4f9914e
choutianxius <choutianxius@gmail.com>
2024-11-16 12:27:29 -0600

    Add ability to suspend/resume game process; implement init game func

--- interface.py
+++ interface.py
@@ -7,6 +7,7 @@ import logging
 import time
 import os
 import pygetwindow as gw
+import keyboard
 
 
 logging.basicConfig(
@@ -16,10 +17,11 @@ logging.basicConfig(
 logger = logging.getLogger("interface")
 
 
-_module_name = "th14.exe"
-_game_title = "Double Dealing Character. ver 1.00b"
+_MODULE_NAME = "th14.exe"
+_GAME_TITLE = "Double Dealing Character. ver 1.00b"
+_FRAME_RATE = 60
 
-offsets = dict(
+_OFFSETS = dict(
     score=0xF5830,
     lives=0xF5864,
     life_pieces=0xF5868,
@@ -33,12 +35,12 @@ offsets = dict(
 
 
 # get a handle to the game window
-_game_windows = gw.getWindowsWithTitle(_game_title)
+_game_windows = gw.getWindowsWithTitle(_GAME_TITLE)
 
 if len(_game_windows) == 1:
     logger.info(f"Found game window: {_game_windows[0]}")
 else:
-    logger.error(f"Cannot find the window with title: {_game_title}")
+    logger.error(f"Cannot find the window with title: {_GAME_TITLE}")
     exit(1)
 
 _game_window = _game_windows[0]
@@ -59,7 +61,7 @@ _module_list = win32process.EnumProcessModules(_module_handle)
 for module in _module_list:
     module_info = win32process.GetModuleFileNameEx(_module_handle, module)
     module_base_name = os.path.basename(module_info)
-    if _module_name.lower() == module_base_name.lower():
+    if _MODULE_NAME.lower() == module_base_name.lower():
         _base_address = module
         break
 
@@ -81,6 +83,14 @@ _process_handle = ctypes.windll.kernel32.OpenProcess(
 )
 
 
+def _suspend_game_process():
+    ctypes.windll.kernel32.DebugActiveProcess(_game_pid)
+
+
+def _resume_game_process():
+    ctypes.windll.kernel32.DebugActiveProcessStop(_game_pid)
+
+
 def _read_game_memory(offset, size):
     buffer = ctypes.create_string_buffer(size)
     bytesRead = ctypes.c_int()
@@ -99,12 +109,67 @@ def _read_game_memory(offset, size):
     return buffer.raw
 
 
+def _press_and_release(key):
+    keyboard.press(key)
+    time.sleep(1 / _FRAME_RATE)
+    keyboard.release(key)
+    time.sleep(1 / _FRAME_RATE)
+
+
+def _get_focus():
+    _game_window.activate()
+    time.sleep(0.2)
+
+
+def _init():
+    """
+    Enter the "practice start" phase from the title screen
+    """
+    _get_focus()
+
+    # after some time of inactivity, the game will enter a demo play phase
+    # and we quit from that
+    _press_and_release("down")
+    _press_and_release("esc")
+    time.sleep(3)
+
+    # now we are on the title screen
+    _press_and_release("down")
+    _press_and_release("esc")
+
+    # now the cursor is at the last line in the menu
+    _press_and_release("down")
+    _press_and_release("down")
+    _press_and_release("z")
+    time.sleep(1)
+
+    # now we are on the game difficulty and character selection screen
+    # notice that the character and difficulty are memorized by the game
+    # and we assume that the proper selections have been made in previous runs
+    _press_and_release("z")
+    time.sleep(1)
+    _press_and_release("z")
+    time.sleep(1)
+    _press_and_release("z")
+    time.sleep(1)
+    _press_and_release("z")
+    time.sleep(2.5)
+
+    _suspend_game_process()
+
+
+def read_game_status_int(key: str):
+    offset = _OFFSETS[key]
+    return _read_game_memory(offset, 4)
+
+
 if __name__ == "__main__":
     try:
+        _init()
         while True:
             status = {}
-            for v in offsets:
-                v_offset = offsets[v]
+            for v in _OFFSETS:
+                v_offset = _OFFSETS[v]
                 data = _read_game_memory(v_offset, 4)
                 if data is not None:
                     status[v] = int.from_bytes(data, byteorder="little")
@@ -113,5 +178,5 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         logger.info("Quitting...")
     finally:
+        _resume_game_process()
         win32api.CloseHandle(_process_handle)
-        win32api.CloseHandle(_module_handle)
